2e79e1f62b8a8c5dea7cd959dcb13e6152bd7a30
Wignyo <wiknyo22@gmail.com>
2025-11-23 07:02:10 +0700

    Fix admin url ordering

--- config/urls.py
+++ config/urls.py
@@ -5,29 +5,21 @@ from django.contrib import admin
 from django.urls import path, include
 from accounting.views import index # Import view tadi
 from django.urls import re_path
-
+from accounting.views import index
 # --- Import "Gerbang" Login Kustom & Refresh JWT ---
 from rest_framework_simplejwt.views import TokenRefreshView
 from users.views import MyTokenObtainPairView # <-- Import "Gerbang" Kustom kita
 # --- BATAS IMPORT ---
 
 urlpatterns = [
+    # --- 1. JALUR ADMIN (PRIORITAS UTAMA) ---
     path('admin/', admin.site.urls),
-    
-    # --- URL "BENTENG" ---
-    
-    # 1. URL untuk Login (Dapetin Token)
-    path('api/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    
-    # 2. URL untuk Refresh Token
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    
-    # 3. URL untuk Registrasi (dari app 'users' kita)
-    path('api/users/', include('users.urls')),
-    
-    # --- URL AKUNTANSI ---
-    # Ini "nangkep" SEMUA URL lain (Laporan, Inventori, dll)
-    # dari file accounting/urls.py
+
+    # --- 2. JALUR API (PRIORITAS KEDUA) ---
     path('api/', include('accounting.urls')),
-    re_path(r'^.*$', index),
+    # path('api/users/', include('users.urls')), # Kalau ada app users
+
+    # --- 3. JALUR REACT (PENAMPUNG TERAKHIR / CATCH-ALL) ---
+    # Taruh ini PALING BAWAH. Jangan ada path lain di bawah baris ini.
+    re_path(r'^.*$', index), 
 ]
